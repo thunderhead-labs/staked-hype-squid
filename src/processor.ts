ab06ad4d4f5f01e753ed00384bbd663eeb616c5b
tom <tom@thunderhead.xyz>
2024-10-30 21:58:41 +0200

    x

--- src/processor.ts
+++ src/processor.ts
@@ -33,16 +33,16 @@ export const processor = new EvmBatchProcessor()
     })
     .setBlockRange({
         from: 8412669,
-        //to: 9098305
+        to: 9098305
     })
     .includeAllBlocks({
         from: 8412669,
-        // to: 9098305
+        to: 9098305
     })
     .addTransaction({
         to: ['0xD53902aDB00ae787fD7d63c97F6213327c0fC38A'],
-        traces: true,
-        logs: true,
+        // traces: true,
+        // logs: true,
     })
 
 export type Fields = EvmBatchProcessorFields<typeof processor>
