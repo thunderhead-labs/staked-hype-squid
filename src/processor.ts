b8fff5cace56c3f7ec1143f2c59f6551730226c2
tom <tom@thunderhead.xyz>
2024-10-31 08:14:21 +0200

    x

--- src/processor.ts
+++ src/processor.ts
@@ -32,13 +32,13 @@ export const processor = new EvmBatchProcessor()
         },
     })
     .setBlockRange({
-        from: 8413669,
+        from: 1000000,
         to: 9715223
         // from: 9638858,
         // to: 9714899
     })
     .includeAllBlocks({
-        from: 8413669,
+        from: 1000000,
         to: 9715223
         // from: 9638858,
         // to: 9714899
