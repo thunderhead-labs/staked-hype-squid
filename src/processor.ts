3197ea2bf3d32ee22d0087e0b65fd79e402410c6
tom <tom@thunderhead.xyz>
2024-10-31 19:33:34 +0200

    x

--- src/processor.ts
+++ src/processor.ts
@@ -22,7 +22,7 @@ export const processor = new EvmBatchProcessor()
         // More RPC connection options at https://docs.subsquid.io/evm-indexing/configuration/initialization/#set-data-source
         rateLimit: 15
     })
-    .setFinalityConfirmation(10)
+    .setFinalityConfirmation(20)
     .setFields({
         transaction: {
             from: true,
